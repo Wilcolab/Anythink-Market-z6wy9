9113c99a47100dee63dfde45ed02c4940a2fadec
Mohammad Umar <umarmohammad582@gmail.com>
2022-12-30 23:35:48 -0500

    Implement search box on frontend

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,7 +1,17 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent"
 
-const Banner = () => {
+const Banner = (props) => {
+  const onSearchChange = (event) => {
+    props.onSearchFilter (
+      event.target.value, 
+      (page) => 
+        agent.Items.byTitle(event.target.value),
+        agent.Items.byTitle(event.target.value)
+      
+    );
+  };
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -9,13 +19,8 @@ const Banner = () => {
         <div>
           <span id="get-part">A place to get</span>
           <form>
-            <input
-              style={{ width: "250px" }}
-              type="text"
-              placeholder="What is it that you truly desire?"
-              name="item"
-              id="search=box"
-            />
+            <input style={{width:"250px"}} type="text" placeholder="What is it that you truly desire?" 
+            name="item" id="search=box" onChange={onSearchChange}/>
           </form>
           <span> the cool stuff.</span>
         </div>
